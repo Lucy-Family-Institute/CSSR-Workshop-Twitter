bfeccac5d8fa84a6b091704c460e6e060905ed59
yangxu698 <yangxu698@gmail.com>
2022-09-05 15:19:36 -0400

    switch to debug queue

--- Code/batch_pull.sh
+++ Code/batch_pull.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -M yxu6@nd.edu       # Email address for job notification
 #$ -m abe               # Send mail when job begins, ends and aborts
-#$ -q long              # Specify queue
+#$ -q debug # Specify queue
 #$ -pe smp 1            # Specify number of cores to use.
 #$ -N UK-RA # Specify job name
 
